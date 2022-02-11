209989ff958ca4673e2419d84deeffaff898e9e2
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-02-12 00:27:04 +0900

    initial commit

---
+++ src/test/java/LeeJerry/realWorld/RealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package LeeJerry.realWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
