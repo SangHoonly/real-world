d95afb688ae751bd5355b0995f50ac77a4ed6cdd
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-02-26 17:25:38 +0900

    Remove: 메인 메서드 테스트 코드 제거

--- src/test/java/LeeJerry/realWorld/RealWorldApplicationTests.java
+++ src/test/java/LeeJerry/realWorld/RealWorldApplicationTests.java
@@ -1,21 +1,8 @@
 package LeeJerry.realWorld;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class RealWorldApplicationTests {
 
-
-    @Autowired
-    SqlSessionTemplate sqlSessionTemplate;
-
-    @Test
-    public void sqlSessionTemplateSimpleTest() {
-        assertThat(sqlSessionTemplate).isInstanceOf(sqlSessionTemplate.getClass());
-    }
 }
