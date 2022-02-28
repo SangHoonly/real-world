34204f051f34c8efa025a09ffefb6efcb343d6ed
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-02-28 16:31:11 +0900

    CI/CD : deploy.sh 실행 jar 파일 지정 및 current pid 지정되지 않는 문제 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,15 @@
 REPOSITORY=/home/ec2-user/realWorld
 
 echo "> build 파일 복사"
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/build/libs/*SNAPSHOT.jar $REPOSITORY/
 
-echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl realWorld | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl realWorld)
+echo "> 현재 실행중인 애플리케이션 pid 확인: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없습니다!"
 else
+  echo "> 현재 애플리케이션이 실행 중인 애플리케이션을 먼저 종료합니다."
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
@@ -20,5 +21,5 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 chmod +x $JAR_NAME
 
-echo "> 새 애플리케이션 실행"
+echo "> 새 애플리케이션을 실행합니다."
 nohup java -jar $JAR_NAME > /home/ec2-user/realWorld/nohup.out 2>&1 &
