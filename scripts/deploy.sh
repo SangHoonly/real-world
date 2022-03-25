8593c6845e4a20a507e295853476045d084c2690
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-03-25 17:48:43 +0900

    배포 스크립트 : 사용자이름, 위치 로그 출력

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,8 @@ cp $REPOSITORY/build/libs/*SNAPSHOT.jar $REPOSITORY/
 
 CURRENT_PID=$(pgrep -fl realWorld)
 echo "> 현재 실행중인 애플리케이션 pid 확인: $CURRENT_PID"
+echo "> 사용자이름 $USER"
+echo "> 현재 위치 $PWD"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없습니다!"
